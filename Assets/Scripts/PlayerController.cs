0c90cf958810db06809f97f02127f5137c56fe30
cromeero <Cromero24@aoiths.org>
2022-09-16 12:09:19 -0400

    1.2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,12 +7,13 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Move the vechicle forward
+      transform.Translate(Vector3.forward * Time.deltaTime * 20); 
     }
 }
